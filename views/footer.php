2d91dea0a0435c6ad03b0edf10f380c625c4b3e0
Adarsh kumar jain <adarshjain583@gmail.com>
2017-02-25 08:49:52 +0000

    As all ajax request completes, show table

---
+++ views/footer.php
@@ -0,0 +1,11 @@
+</div>
+
+            <div id="bottom">
+                Brought to you by <a href="http://sophomores.in">Sophomores</a>.
+            </div>
+
+        </div>
+
+    </body>
+
+</html>
---
+++ views/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <!-- http://getbootstrap.com/ -->
+        <link href="/css/bootstrap.min.css" rel="stylesheet"/>
+
+        <link href="/css/styles.css" rel="stylesheet"/>
+
+        <title>Scrapping</title>
+
+        <!-- https://jquery.com/ -->
+        <script src="/js/jquery-1.11.3.min.js"></script>
+
+        <!-- http://getbootstrap.com/ -->
+        <script src="/js/bootstrap.min.js"></script>
+
+        <script src="/js/scripts.js"></script>
+
+    </head>
+
+    <body>
+
+        <div class="container">
+
+            <div id="top">
+                <div>
+                    <a href="/"><img alt="Shiksha.com" src="/img/logo.png"/></a>
+                </div>
+                
+                
+            </div>
+
+            <div id="middle">
